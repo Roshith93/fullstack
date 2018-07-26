f48955092f66ab4bc3e0d7bc885d986d97aee565
roshith R <roshith.r@valueleaf.com>
2018-07-26 18:47:48 +0530

    passport authentication and google auth  completely done

--- index.js
+++ index.js
@@ -16,12 +16,19 @@ passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-  console.log(accessToken);
+}, (accessToken,refreshToken,profile,done) => {
+  console.log('access token',accessToken);
+  console.log('refresh token',refreshToken);
+  console.log('profile',profile);
 }));
 
+// route handler for google auth
 app.get('/auth/google', passport.authenticate('google',{
   scope : ['profile', 'email']
 }));
+
+// Call back url route handler
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
