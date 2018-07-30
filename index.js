b2da359ff57f29254a9bd56441d461f433a74ca5
roshith R <roshith.r@valueleaf.com>
2018-07-30 10:03:36 +0530

    server structure refactor - reaaranged details for security and all the credentials stored separetly

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-// Route Handler
-
-// app.get('/', (req,res)=>{
-//   res.send({hi:'there', message : "Fuck you bitch"});
-
-// });
-
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken,refreshToken,profile,done) => {
-  console.log('access token',accessToken);
-  console.log('refresh token',refreshToken);
-  console.log('profile',profile);
-}));
-
-// route handler for google auth
-app.get('/auth/google', passport.authenticate('google',{
-  scope : ['profile', 'email']
-}));
-
-// Call back url route handler
-app.get('/auth/google/callback', passport.authenticate('google'));
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+
+    // route handler for google auth
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    // Call back url route handler
+    app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+}
